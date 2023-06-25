2b4a2ee8091c42959afa108062ec2726ab262037
Jaroslav Bezdek <jaroslav.bezdek@strv.com>
2023-06-25 17:48:54 +0200

    feat: add data dictionary

--- README.md
+++ README.md
@@ -21,6 +21,28 @@ is used. To start it, please, follow the next steps.
 1. Run docker container: `docker run -it -p 8888:8888 -v $(pwd):/usr/src/app ds-nhl:latest /bin/bash`
 1. Run notebook inside the docker container: `jupyter notebook --ip 0.0.0.0 --no-browser --allow-root`
 
-### :floppy_disk: Data download
+## :floppy_disk: Data
 
-Data were downloaded from the [MoneyPuck.com](https://moneypuck.com/data.htm) website.
+> No guarantees are made to the quality of the data. NHL data is known to have issues and biases.
+
+The whole analytics part is based on data from the [MoneyPuck.com](https://moneypuck.com/data.htm)
+website. The data can be downloaded by running the following command in the docker container.
+
+```bash
+python /usr/src/app/scripts/download_data.py
+```
+
+The [/scripts/download_data.py](./scripts/download_data.py) script creates a new `/data` folder
+in the project root directory, downloads data, and organizes the files within the folder.
+
+### :book: Data dictionary
+
+Data dictionary can be found in the [/data_dictionary.json](./data_dictionary.json) file.
+The dictionary is divided into two sections: players, and shots. The first section covers all the
+columns related to skaters, goalies, lines, and teams. The second section describes the shots
+datasets columns.
+
+The minor purpose of the data dictionary is to create a mapper between the
+[MoneyPuck.com](https://moneypuck.com/data.htm) original data columns and our own column names.
+This mapper is used for renaming and filtering the original columns during the data download,
+implemented inside the [/scripts/download_data.py](./scripts/download_data.py) script.
--- scripts/download_data.py
+++ scripts/download_data.py
@@ -1,8 +1,9 @@
 """
-Script that downloads data, and organizes the downloaded files into folders structure.
+Script that downloads data, and organizes the downloaded files into corresponding folders.
 """
 
 import io
+import json
 import zipfile
 from itertools import product
 from pathlib import Path
@@ -12,17 +13,80 @@ import requests
 
 DATA_SOURCE_URL = "https://moneypuck.com/moneypuck/playerData/seasonSummary"
 DATA_FOLDER = Path("/usr/src/app/data/")
+DATA_DICT_PATH = Path("/usr/src/app/data_dictionary.json")
 
 STAT_TYPES = ("skaters", "goalies", "teams", "lines")
 SEASON_YEARS = (2018, 2019, 2020, 2021, 2022)
 SEASON_PARTS = ("regular", "playoffs")
 
 
-def download_statistics() -> None:
+def get_col_mappers() -> dict:
+    """Return a nested dictionary where the keys represent different data types and the values are
+    dictionaries that map MoneyPuck column names to corresponding new column names within each data
+    type.
+
+    Use data from the data dictionary JSON file, saved in the project root directory.
+
+    Returns
+    -------
+        dict
+    """
+    with open(file=DATA_DICT_PATH, mode="r", encoding="utf-8") as data_dict_file:
+        data_dict = json.load(data_dict_file)
+
+    return {
+        data_type: {col.get("column_moneypuck"): col.get("column") for col in cols}
+        for data_type, cols in data_dict.items()
+    }
+
+
+def download_csv_file(
+    source_filepath: str,
+    destination_filepath: str,
+    moneypuck_col_to_new_col: dict,
+    storage_options={"User-Agent": "Mozilla/5.0"},
+) -> None:
+    """Download a CSV file from the specified filepath/URL address, rename the columns using
+    the provided mapping dictionary, and save the modified CSV file to the specified filepath.
+
+    Parameters
+    ----------
+    source_filepath : str
+        The filepath, or URL from which to download the CSV file.
+    moneypuck_col_to_new_col : dict
+        A dictionary that maps MoneyPuck column names to new column names.
+    destination_filepath : str
+        The path where the modified CSV file will be saved.
+    storage_options : dict
+        Extra options that make sense for a particular storage connection, e.g. host, port,
+        username, password, etc. For HTTP(S) URLs the key-value pairs are forwarded
+        to urllib.request.Request as header options. For other URLs (e.g. starting with “s3://”,
+        and “gcs://”) the key-value pairs are forwarded to fsspec.open.
+
+    Returns
+    -------
+    None
+        This function does not return anything directly, but saves the modified CSV file to the
+        specified filepath.
+    """
+    return (
+        pd.read_csv(source_filepath, storage_options=storage_options)
+        .rename(columns=moneypuck_col_to_new_col)
+        .filter(items=moneypuck_col_to_new_col)
+        .to_csv(destination_filepath, index=False)
+    )
+
+
+def download_player_statistics(moneypuck_col_to_new_col: dict) -> None:
     """Download files with statistics for each statistics type, season year and season part
-    (regular, or play-off) from a specified URL and save the files in the corresponding folder.
+    from a specified URL and save the files in the corresponding folder.
 
     Statistics SHOULD be available for the current season going back to the 2008/2009 season.
+
+    Parameters
+    ----------
+    moneypuck_col_to_new_col : dict
+        A dictionary that maps MoneyPuck column names to new column names.
 
     Returns
     -------
@@ -39,18 +103,25 @@ def download_statistics() -> None:
         filepath = DATA_FOLDER / stat_type / f"{season_year}_{season_part}.csv"
 
         if not filepath.is_file():
-            df = pd.read_csv(url, storage_options={"User-Agent": "Mozilla/5.0"})
-            df.to_csv(filepath, index=False)
-
+            download_csv_file(
+                source_filepath=url,
+                destination_filepath=filepath,
+                moneypuck_col_to_new_col=moneypuck_col_to_new_col,
+            )
             print(f"✅ downloaded {stat_type} data from the {season_year} {season_part} season")
 
 
-def download_shots() -> None:
+def download_shots(moneypuck_col_to_new_col: dict) -> None:
     """Download and extract compressed CSV files containing shots data from multiple seasons.
 
     The files are downloaded from a specific URL for each season and saved in the '/data/shots'
     folder. After extraction, the function renames the files by removing the "shots_" substring
     from their names.
+
+    Parameters
+    ----------
+    moneypuck_col_to_new_col : dict
+        A dictionary that maps MoneyPuck column names to new column names.
 
     Returns
     -------
@@ -62,29 +133,72 @@ def download_shots() -> None:
 
     # download, and extract compressed CSV files
     for season_year in SEASON_YEARS:
-        url = f"https://peter-tanner.com/moneypuck/downloads/shots_{season_year}.zip"
+        shots_file_path = shots_folder / f"{season_year}.csv"
+        if shots_file_path.exists():
+            continue
 
-        response = requests.get(url=url)
+        response = requests.get(
+            url=f"https://peter-tanner.com/moneypuck/downloads/shots_{season_year}.zip"
+        )
+
         with zipfile.ZipFile(io.BytesIO(response.content), "r") as file:
             file.extractall(path=shots_folder)
 
+    # iterate over files, remove "shots_" substring from the file name, and modify CSV
+    for old_file_path in shots_folder.iterdir():
+        old_file_name = old_file_path.name
+
+        if "shots_" in old_file_name:
+            new_file_name = old_file_name.replace("shots_", "")
+            new_file_path = old_file_path.rename(shots_folder / new_file_name)
+
+            download_csv_file(
+                source_filepath=new_file_path,
+                destination_filepath=new_file_path,
+                moneypuck_col_to_new_col=moneypuck_col_to_new_col,
+                storage_options=None,
+            )
             print(f"✅ downloaded shots data from the {season_year} season")
 
-    # remove "shots_" substring from files names
-    for file_path in shots_folder.iterdir():
-        old_file_name = file_path.name
-        new_file_name = old_file_name.replace("shots_", "")
-        file_path.rename(shots_folder / new_file_name)
 
-        print(f"✅ renamed '{old_file_name}' to '{new_file_name}'")
+def download_players_lookup(moneypuck_col_to_new_col: dict) -> None:
+    """Download all players lookup table and the file in the /data folder.
+
+    Parameters
+    ----------
+    moneypuck_col_to_new_col : dict
+        A dictionary that maps MoneyPuck column names to new column names.
+
+    Returns
+    -------
+        None
+    """
+    url = "https://moneypuck.com/moneypuck/playerData/playerBios/allPlayersLookup.csv"
+    filepath = DATA_FOLDER / "players_lookup.csv"
+
+    if not filepath.is_file():
+        download_csv_file(
+            source_filepath=url,
+            destination_filepath=filepath,
+            moneypuck_col_to_new_col=moneypuck_col_to_new_col,
+        )
+        print("✅ downloaded players lookup table")
 
 
-def main():
-    """Create /data folder and download data."""
+def main() -> None:
+    """Create /data folder and download data.
+
+    Returns
+    -------
+        None
+    """
     Path(DATA_FOLDER).mkdir(parents=True, exist_ok=True)
 
-    download_statistics()
-    download_shots()
+    col_mappers = get_col_mappers()
+
+    download_player_statistics(moneypuck_col_to_new_col=col_mappers.get("players"))
+    download_shots(moneypuck_col_to_new_col=col_mappers.get("shots"))
+    download_players_lookup(moneypuck_col_to_new_col=col_mappers.get("players_lookup"))
 
     print("🎉 all data downloaded successfully")
 
