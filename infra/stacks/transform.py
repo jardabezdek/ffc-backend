69cef9118355a48f6029a84a07dec01d22784d14
Jaroslav Bezdek <jaroslav.bezdek@strv.com>
2024-01-02 15:39:06 +0100

    refactor(infra): transform stack

--- infra/stacks/transform.py
+++ infra/stacks/transform.py
@@ -1,6 +1,14 @@
 from pathlib import Path
 
-from aws_cdk import Stack, aws_ec2, aws_ecr_assets, aws_ecs, aws_iam, aws_lambda
+from aws_cdk import (
+    Stack,
+    aws_ec2,
+    aws_ecr_assets,
+    aws_ecs,
+    aws_events,
+    aws_events_targets,
+    aws_iam,
+)
 from constructs import Construct
 from stacks.utils import get_name
 
@@ -8,48 +16,84 @@ from stacks.utils import get_name
 class Transform(Stack):
     """Stack used for dbt transformation."""
 
+    frequency_cron_transform = {"minute": "15", "hour": "7"}
+    task_cpu = "1024"
+    task_memory_mib = "2048"
+
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # create VPC
-        self.vpc_transform = aws_ec2.Vpc(
+        self.vpc = self.create_vpc()
+        self.cluster = self.create_ecs_cluster()
+        self.image = self.create_image()
+        self.task = self.create_task()
+
+    def create_vpc(self) -> aws_ec2.Vpc:
+        """Create a Virtual Private Cloud for the transformation stack.
+
+        Returns:
+        --------
+        aws_ec2.Vpc
+            The configured VPC.
+        """
+        return aws_ec2.Vpc(
             self,
             id="VpcTransform",
             vpc_name=get_name("vpc-transform"),
-            nat_gateways=2,
             max_azs=2,
         )
 
-        # create security group
-        self.security_group_transform = aws_ec2.SecurityGroup(
-            self,
-            id="SecurityGroupTransform",
-            vpc=self.vpc_transform,
-            security_group_name=get_name("security-group-transform"),
-            allow_all_outbound=True,
-        )
+    def create_ecs_cluster(self) -> aws_ecs.Cluster:
+        """Create an Elastic Container Service cluster for the transformation stack.
 
-        # create ECS cluster to deploy tasks to
-        self.ecs_cluster = aws_ecs.Cluster(
+        Returns:
+        --------
+        aws_ecs.Cluster
+            The configured ECS cluster for transformation.
+        """
+        return aws_ecs.Cluster(
             self,
             id="ECSClusterTransform",
             cluster_name=get_name("ecs-cluster-transform"),
             container_insights=True,
-            vpc=self.vpc_transform,
+            vpc=self.vpc,
         )
 
-        # add ECS task for defining provisioning of resources
-        self.ecs_task_runner = aws_ecs.TaskDefinition(
+    def create_image(self):
+        """Create a Docker image asset for the ECR repository in the transformation stack.
+
+        Returns:
+        --------
+        aws_ecr_assets.DockerImageAsset
+            The Docker image asset for the ECR repository.
+        """
+        return aws_ecr_assets.DockerImageAsset(
+            self,
+            id="ECRImageTransform",
+            asset_name=get_name("ecr-image-transform"),
+            directory=(Path(__file__).resolve().parent.parent / "transform").as_posix(),
+            platform=aws_ecr_assets.Platform.LINUX_AMD64,
+        )
+
+    def create_task(self):
+        """Create an Elastic Container Service task definition for transformation.
+
+        Returns:
+        --------
+        aws_ecs.TaskDefinition
+            The ECS task definition for the transformation task.
+        """
+        task = aws_ecs.TaskDefinition(
             self,
             id="ECSTaskDefinitionTransform",
             family=get_name("ecs-task-runner-transform"),
             compatibility=aws_ecs.Compatibility.FARGATE,
-            cpu="1024",
-            memory_mib="2048",
+            cpu=self.task_cpu,
+            memory_mib=self.task_memory_mib,
         )
 
-        # add ECS task permissions to read/write S3 objects
-        self.ecs_task_runner.add_to_task_role_policy(
+        # add ECS task permissions to communicate with S3
+        task.add_to_task_role_policy(
             statement=aws_iam.PolicyStatement(
                 actions=[
                     "ecs:StartTelemetrySession",
@@ -60,46 +104,23 @@ class Transform(Stack):
             )
         )
 
-        # create image
-        self.ecr_image_transform = aws_ecr_assets.DockerImageAsset(
-            self,
-            id="ECRImageTransform",
-            asset_name=get_name("ecr-image-transform"),
-            directory=(Path(__file__).resolve().parent.parent / "transform").as_posix(),
-            platform=aws_ecr_assets.Platform.LINUX_AMD64,
-        )
-
         # add the docker container to the task
-        self.ecs_container = self.ecs_task_runner.add_container(
+        task.add_container(
             id="ECSContainerTransform",
             container_name=get_name("ecs-container-transform"),
-            image=aws_ecs.ContainerImage.from_docker_image_asset(asset=self.ecr_image_transform),
+            image=aws_ecs.ContainerImage.from_docker_image_asset(asset=self.image),
             logging=aws_ecs.LogDriver.aws_logs(stream_prefix=get_name("ecs-log-transform")),
         )
 
-        # create a lambda function used to trigger the task to start
-        self.lambda_trigger = aws_lambda.Function(
+        # schedule ECS task to run on regular basis
+        event_rule = aws_events.Rule(
             self,
-            id="LambdaTransformTrigger",
-            function_name=get_name("trigger-transform"),
-            description="Trigger dbt transformation.",
-            runtime=aws_lambda.Runtime.PYTHON_3_12,
-            code=aws_lambda.Code.from_asset(
-                (Path(__file__).resolve().parent / "lambdas" / "trigger-dbt-transform").as_posix()
-            ),
-            handler="function.handler",
-            environment={
-                "ECS_CLUSTER_ARN": self.ecs_cluster.cluster_arn,
-                "ECS_TASK_DEFINITION_ARN": self.ecs_task_runner.task_definition_arn,
-                "ECS_CONTAINER_NAME": self.ecs_container.container_name,
-                "ECS_SUBNET_IDS": ",".join(
-                    self.vpc_transform.select_subnets(
-                        subnet_type=aws_ec2.SubnetType.PRIVATE_WITH_NAT
-                    ).subnet_ids
-                ),
-                "ECS_SECURITY_GROUP_ID": self.security_group_transform.security_group_id,
-            },
+            id="TransformScheduleRule",
+            rule_name=get_name("rule-transform"),
+            schedule=aws_events.Schedule.cron(**self.frequency_cron_transform),
+        )
+        event_rule.add_target(
+            target=aws_events_targets.EcsTask(cluster=self.cluster, task_definition=task)
         )
 
-        # grant lambda function the permissions to run task
-        self.ecs_task_runner.grant_run(grantee=self.lambda_trigger)
+        return task
