975b9ebaca2a348caf608c4ca1443dba8c691987
Jaroslav Bezdek <jaroslav.bezdek@strv.com>
2024-01-11 17:08:51 +0100

    feat(transform): set base_games as incremental

--- transform/models/base/base_games.sql
+++ transform/models/base/base_games.sql
@@ -2,6 +2,14 @@ with base as (
 
     select * from {{ source("base", "games") }}
 
+),
+
+base_incremental as (
+
+    select * from {{ source("base_incremental", "games") }}
+
 )
 
 select * from base
+union
+select * from base_incremental
