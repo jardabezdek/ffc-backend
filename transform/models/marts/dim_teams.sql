29cafa37bd665938a6124c7f483c921f575b85df
Jaroslav Bezdek <bezdekjarda@seznam.cz>
2024-07-13 11:00:50 +0200

    fix(transform): add Utah team

--- transform/models/marts/dim_teams.sql
+++ transform/models/marts/dim_teams.sql
@@ -12,6 +12,21 @@ team_ids as (
 
     from {{ ref("base_games") }}
 
+),
+
+utah as (
+	
+    select
+        59 as id,
+        'Utah Hockey Club' as team_full_name,
+        'UTA' as team_abbrev_name,
+        'Utah' as team_common_name,
+        'Western' as conference,
+        'W' as conference_abbrev,
+        'Central' as division,
+        'C' as division_abbrev,
+        'https://assets.nhle.com/logos/nhl/svg/UTA_light.svg' as team_logo_url
+
 )
 
 select 
@@ -23,4 +38,6 @@ from teams
 left join team_ids
   on teams.team_abbrev_name = team_ids.team_abbrev
 
-order by 1
+-- order by 1
+
+union select * from utah
